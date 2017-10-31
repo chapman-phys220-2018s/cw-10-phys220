d126ca36182619a17435557b3d32f07eb418b4a7
Justin Dressel <dressel@chapman.edu>
2017-10-31 08:50:01 +0000

    2017 update

--- README.md
+++ README.md
@@ -1,22 +1,14 @@
 # PHYS220 CW 10 
 
-**Author(s):** _\<your name(s)\>_
+**Author(s):** **CHANGEME**
 
-[![Build Status](https://travis-ci.org/chapman-phys220-2016f/cw-10-YOURNAME.svg?branch=master)](https://travis-ci.org/chapman-phys220-2016f/cw-10-YOURNAME)
-
-**Due date:** 2016/11/08
+[![Build Status](https://travis-ci.org/chapman-phys220-2017f/cw-10-YOURNAME.svg?branch=master)](https://travis-ci.org/chapman-phys220-2017f/cw-10-YOURNAME)
 
 ## Specification
 
-**Reminder: We have switched to Python3 officially.**
-
-Complete the following exercises from the primary textbook, placing your solutions into separate files. In each file, write the solution as a callable function, so that you can write suitable test functions that demonstrate correct output using the nose framework. GitHub will automatically run your tests on every commit, indicating any failures via the Travis framework with build status above.
-
-1. p.508, 8.15, in ```flip_coin_vec.py```. Learning to vectorize conditionals using numpy is this way is a useful prototype for the remainder of the assignment.
-1. Re-implement the code in ```walk2Dv.py``` shown on p.497 of the textbook. That is, fix this code so that it is correctly formatted, commented, and uses a proper ```__main__``` block for running it as a script on the command line. Use better (more descriptive) variable names, and generally clean up the algorithm.
-1. The output of the random walk code is a sequence of .pdf files that you can later collect into animated gifs using a simple bash command: ```convert -delay 50 -loop 0 tmp_*.pdf walk.gif```  This command uses the ImageMagick file conversion suite in linux, which is also installed in your Sage Cloud account. The delay parameter specifies frame delay in milliseconds. The loop parameter of 0 specifies an infinite number of loops for the gif. Create such an animated gif for the 2D random walk and commit it to the repository. (Do not commit any individual pdf frames.) Using 1000 particles that walk for 400 steps.
-
-To cleanly present your work, create a Jupyter notebook ```cw8-randomwalk.ipynb``` that imports each file and carefully discusses the algorithms being implemented. The notebook should be formatted professionally using Markdown headings, including your name and a brief description of each homework section. 
+1. Go through the example notebook and code with your group. Discuss and make sure you understand what is happening.
+1. Create a new module `estimate_e.py`. Using the fact that $\int_0^1 e^x dx = e - 1$, estimate the value of $e$ using a Monte Carlo technique. (Hint: think about how $\pi$ is estimated in the notebook.) Test your implementation using a test module `test_estimate_e.py` that passes with Travis.
+1. Complete the exercise at the bottom of the notebook. Be sure to preserve the existing code such that the rest of the notebook still runs unmodified.
 
 Pro-tip: using git to manage conflicts on Jupyter notebooks is a pain. I recommend delegating one person from your group to edit the notebook, to avoid merge conflicts.
 
---
+++ randomwalk.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation, rc
+from IPython.display import HTML
+
+"""2D Random walk library
+
+This module introduces Monte Carlo techniques by showing how to simulate
+many particles that randomly walk around a 2D lattice. This is a crude but
+effective model for heat transfer, as well as air diffusion.
+"""
+
+def walk_gen(walkers=10, steps_per_frame=1):
+    """Generator for 2D random walkers
+    Yields x and y coordinate arrays for particles that randomly walk around
+    a 2D lattice indexed by integers. Each walker may move in one of four
+    cardinal directions randomly, on each time step. The generator yields all
+    new positions with each iteration.
+    
+    Args:
+      walkers : int, number of walkers
+      steps_per_frame : number of internal steps to take before each yield
+    
+    Returns:
+      (xs, ys) : tuple of arrays of x and y coordinate integers for all walkers
+    """
+    xs = np.zeros(walkers)
+    ys = np.zeros(walkers)
+    EAST, WEST, NORTH, SOUTH = 0, 1, 2, 3 
+    while True:
+        for step in range(steps_per_frame):
+            moves = np.random.randint(4, size=walkers)
+            xs += np.where(moves == EAST, 1, 0)
+            xs -= np.where(moves == WEST, 1, 0)
+            ys += np.where(moves == NORTH, 1, 0)
+            ys -= np.where(moves == SOUTH, 1, 0)
+        yield (xs,ys)
+
+def plot_anim(frame_gen, xlim=(-30,30), ylim=(-30,30), delay=20, max_frames=100,
+                   title=None, xlabel=None, ylabel=None, gif=False):
+    """Produce a scatterplot point animation from a frame-generating function. 
+    Works in two modes:
+      - Return a Jupyter HTML5 wrapper around a rendered mp4 video of the animation
+      - Create an animated gif file of the animation
+    The first mode is default and recommended for in-notebook rendering.
+    
+    Args:
+      frame_gen : Generator that yields successive tuples (xs, ys) of 
+                  points, as domain and range arrays, to plot as frames.
+                  The frames will continue until max_frames is reached.
+      xlim = (xmin,xmax) : Horizontal plot range 
+      ylim = (ymin,ymax) : Vertical plot range  
+      delay : number of ms between frames
+      max_frames : maximum number of saved frame 
+      title : plot title (optional)
+      xlabel : plot x axis label (optional)
+      ylabel : plot y axis label (optional)
+      gif : Boolean, if true render gif file instead of outputting HTML5 
+    
+    Returns:
+      HTML object containing mp4 video of animation (when gif false)
+    
+    Effects:
+      Saves a gif file containing the animation (when gif true)
+    """
+    # Create empty plot set to desired fixed zoom
+    fig, ax = plt.subplots()
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+    if title:  plt.title(title)
+    if xlabel: plt.xlabel(xlabel)
+    if ylabel: plt.ylabel(ylabel)
+    
+    # Draw an empty line and save the handle to update later
+    line, = ax.plot([], [], 'r.', alpha=0.4)
+    
+    # Define how to generate a blank frame
+    def init_frame():
+        line.set_data([],[])
+        return (line,)
+    
+    # Define how to update a frame 
+    def animate(i):
+        x,y = next(frame_gen)
+        line.set_data(x,y)
+        return (line,)
+    
+    # Define animation object using frame generator
+    # Use blit to redraw only the changes that were made
+    anim = animation.FuncAnimation(fig, animate, init_func=init_frame, 
+                                   frames=range(max_frames), interval=delay, 
+                                   save_count=max_frames, blit=True)
+    
+    # Tidy up stray plot within the notebook itself
+    plt.close()
+    
+    if gif:
+        # Render animation as animated gif file
+        anim.save(frame_gen.__name__+'.gif', writer='imagemagick')
+    else:
+        # Make sure that animation renders to HTML5 by default
+        rc('animation', html='html5')
+        # Convert animation to HTML5 and return
+        return HTML(anim.to_html5_video())
