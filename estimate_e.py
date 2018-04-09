c2dbde4bc14d5ca1329c7374312e69ce0656ee33
Daniel <Chang254@mail.chapman.edu>
2018-04-09 20:26:43 +0000

    Completed exercise 2

---
+++ estimate_e.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+###
+#Daniel Chang
+#2260161
+#Chang254@mail.chapman.edu
+#PHYS220 Spring 2018
+#CW10
+###Exercise 2
+
+import numpy as np
+
+def estimate_integral(f,a,b,N=100000):
+    xs = np.random.uniform(a,b,N)
+    return f(xs).sum() * (b-a)/N
+
+def est_e(f,a,b,N):
+    return estimate_integral(f,a,b,N) + 1
+    
